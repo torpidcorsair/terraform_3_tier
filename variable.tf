2c91f97769de1a94c912e6f13620e52760ffcc5f
Hypha-cohort-1 <saniak@joinhypha.com>
2024-06-22 02:05:50 +0530

    first commit

---
+++ variable.tf
@@ -0,0 +1,9 @@
+
+variable "static_website_bucket_name" {
+  type    = string
+  default = "my-tf-hypha-ritchie-bucket"
+}
+variable "build-bucker-name" {
+  type    = string
+  default = "hypha-build-and-push-bucket"
+}
