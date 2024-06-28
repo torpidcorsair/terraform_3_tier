39093dece35c336cba64be24ac26399a37274b05
Hypha-cohort-1 <saniak@joinhypha.com>
2024-06-28 21:09:41 +0530

    Update IAMPolicy.tf

--- IAMPolicy.tf
+++ IAMPolicy.tf
@@ -80,8 +80,8 @@ resource "aws_iam_policy" "policy_for_EC2" {
         "Effect": "Allow",
         "Action": ["s3:GetObject",
                    "s3:ListBucket"],
-        "Resource": ["arn:aws:s3:::hypha-saniakurup-bucket/*",
-                     "arn:aws:s3:::hypha-saniakurup-bucket"]
+        "Resource": ["arn:aws:s3:::${var.build-bucker-name}/*",
+                     "arn:aws:s3:::${var.build-bucker-name}"]
       },
       {
         "Effect" : "Allow",
