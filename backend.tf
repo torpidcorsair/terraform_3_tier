b916de9ad51112de38b2b559a50ecc88382224b4
vilasvarghese <49194047+vilasvarghese@users.noreply.github.com>
2024-07-14 17:21:57 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "vilas14ju"
-    key    = "terrform.tfstate" #"your_tf_state_file.tfstate"
+    key    = "your_tf_state_file.tfstate"
     region = "us-east-1"
   }
 }
