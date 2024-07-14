4730ef42f0fab3065020d4e71e4f043b345f353e
vilasvarghese <49194047+vilasvarghese@users.noreply.github.com>
2024-07-14 14:58:20 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,9 +1,9 @@
 
 variable "static_website_bucket_name" {
   type    = string
-  default = "my-tf-saniakurup-bucket"
+  default = "my-tf-vilas10ju-bucket"
 }
 variable "build-bucker-name" {
   type    = string
-  default = "hypha-saniakurup-bucket"
+  default = "hypha-vilas10ju-bucket"
 }
